069b23cc533f00a39f34f08e2a5960460cd02d28
Felipe-stack-jpg <255582023@eniac.edu.br>
2025-07-31 19:17:44 -0300

    criamos a pagina teste do html e css com static, criamos o models e suas migracoes e passamos o django para o horario de SP com a linguagem em portugues!

--- registro/admin.py
+++ registro/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from registro import models
 
-# Register your models here.
+@admin.register(models.registro)
+class registroAdmin(admin.ModelAdmin):
+    list_display = 'id', 'nome', 'sobrenome', 'telefone',
+    ordering = '-id',
+    list_filter = 'data_criacao',
+    search_fields = 'id', 'nome', 'sobrenome', 'telefone', 
--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class registro(models.Model):
+    nome = models.CharField(max_length= 50)
+    sobrenome = models.CharField(max_length= 100)
+    telefone = models.CharField(max_length=15, null=True)
+    email = models.EmailField(unique = True, max_length=254)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
